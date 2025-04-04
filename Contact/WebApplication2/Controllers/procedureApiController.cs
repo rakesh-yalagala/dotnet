d63be7aef42dbae3f582a2763344a9b8eff3eebe
rakesh-yalagala <raleshyalagala7041@gmail.com>
2025-04-04 22:19:51 +0530

    added

--- Contact/WebApplication2/Controllers/procedureApiController.cs
+++ Contact/WebApplication2/Controllers/procedureApiController.cs
@@ -5,11 +5,16 @@ using System.Linq;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Elfie.Diagnostics;
 using Npgsql;
+
 using NpgsqlTypes;
 using WebApplication2.Models;
 using WebApplication2.Models.Response;
+using System.Text.Json;
+using System.Globalization;
+using System.Transactions;
 
 namespace WebApplication2.Controllers
 {
@@ -22,6 +27,91 @@ namespace WebApplication2.Controllers
         {
             _configuration = configuration;
         }
+
+        // [HttpGet("getcheck")]
+        // public IActionResult getcheck(int id)
+        // {
+        //     string? pgsqlConnection = _configuration.GetConnectionString("DefaultConnection");
+        //     using (NpgsqlConnection npgsqlConnection = new NpgsqlConnection(pgsqlConnection))
+        //     {
+        //         npgsqlConnection.Open();
+        //         string? contactName;
+        //         using (var contactCmd = new NpgsqlCommand(@"CALL GetContacts(:p_ContactId,:p_ContactName)", npgsqlConnection))
+        //         {
+        //             contactCmd.Parameters.AddWithValue("p_ContactId", id);
+        //             var contactNameParam = new NpgsqlParameter("p_ContactName", NpgsqlDbType.Varchar)
+        //             {
+        //                 Direction = ParameterDirection.InputOutput,
+        //                 Value = DBNull.Value
+        //             };
+        //             contactCmd.Parameters.Add(contactNameParam);
+
+        //             contactCmd.ExecuteNonQuery();
+        //             contactName = contactNameParam.Value.ToString();
+        //         }
+        //         using (NpgsqlTransaction transaction = npgsqlConnection.BeginTransaction())
+        //         {
+        //             using (NpgsqlCommand cmd = new NpgsqlCommand(@"call GetAllNumbers(:p_ContactId,:Ref_cursor)", npgsqlConnection, transaction))
+        //             {
+        //                 cmd.Parameters.AddWithValue("p_ContactId", NpgsqlDbType.Integer, id);
+        //                 var cursorreference = new NpgsqlParameter("Ref_cursor", NpgsqlDbType.Refcursor)
+        //                 {
+        //                     Direction = ParameterDirection.InputOutput,
+        //                     Value = "getCursor"
+        //                 };
+        //                 cmd.Parameters.Add(cursorreference);
+        //                 cmd.ExecuteNonQuery();
+
+        //                 var NumberDetails = new List<Alternewcontactresponse>();
+        //                 using (var fetchCmd = new NpgsqlCommand("FETCH ALL FROM getCursor;", npgsqlConnection))
+        //                 using (var reader = fetchCmd.ExecuteReader())
+        //                 {
+        //                     while (reader.Read())
+        //                     {
+        //                         NumberDetails.Add(new Alternewcontactresponse
+        //                         {
+        //                             NumberId = reader.GetInt32(1), // NumberId is at index 1
+        //                             PhoneNumber = reader.GetString(0) // PhoneNumber is at index 0
+        //                         });
+        //                     }
+        //                 }
+
+        //                 // cmd.CommandText = "Fetch all in getCursor";
+        //                 // cmd.CommandType = CommandType.Text;
+
+        //                // var NumberDetails = new List<Alternewcontactresponse>();
+        //                 // NpgsqlDataReader dr = cmd.ExecuteReader();
+
+        //                 // while (dr.Read())
+        //                 // {
+        //                 //     NumberDetails.Add(new Alternewcontactresponse
+        //                 //     {
+        //                 //         NumberId = dr.GetInt32(0),
+        //                 //         PhoneNumber = dr.GetString(1)
+        //                 //     }
+        //                 //      );
+        //                 // }
+        //                 // dr.Close();
+        //                 transaction.Commit();
+        //                 npgsqlConnection.Close();
+
+        //                 var contactDetails = new Newcontactresponse
+        //                 {
+        //                     ContactName = contactName,
+        //                     ListOfNumbers = NumberDetails
+        //                 };
+        //                 return Ok(contactDetails);
+        //             }
+
+        //         }
+
+        //     }
+        // }
+
+       
+
+
+        //-----
 
         [HttpPost("createcall")]
         public IActionResult CreateContact([FromBody] NewContacts Contacts)
@@ -77,10 +167,8 @@ namespace WebApplication2.Controllers
                         // .Value = DateTime.SpecifyKind(t.CreatedOn == default(DateTime) ? DateTime.UtcNow : t.CreatedOn, DateTimeKind.Unspecified);
                         var id = new NpgsqlParameter("@rv_NumberId", NpgsqlDbType.Integer)
                         {
-                            Direction = ParameterDirection.InputOutput
-                           ,
+                            Direction = ParameterDirection.InputOutput,
                             Value = DBNull.Value
-
                         };
                         cmd.Parameters.Add(id);
                         cmd.ExecuteNonQuery();
@@ -109,9 +197,9 @@ namespace WebApplication2.Controllers
                 using (var transaction = npgsqlConnection.BeginTransaction())
                 {
 
-                    using (var cmd = new NpgsqlCommand("CALL GetNumber(@p_ContactId, @p_PhoneNumber, @p_NumberId)", npgsqlConnection, transaction))
+                    using (var cmd = new NpgsqlCommand("CALL GetNumbers(@p_ContactId, @p_PhoneNumber, @p_NumberId)", npgsqlConnection, transaction))
                     {
-                        cmd.Parameters.AddWithValue("p_ContactId", id);
+                        cmd.Parameters.AddWithValue("p_ContactId", NpgsqlDbType.Integer, id);
 
 
                         var phoneNumberParam = new NpgsqlParameter("p_PhoneNumber", NpgsqlDbType.Varchar)
@@ -135,7 +223,7 @@ namespace WebApplication2.Controllers
                         int numberId = numberIdParam.Value != DBNull.Value ? Convert.ToInt32(numberIdParam.Value) : 0;
 
                         string contactName = "Not Defined";
-                        using (var contactCmd = new NpgsqlCommand("CALL GetContact(@p_ContactId, @p_ContactName)", npgsqlConnection, transaction))
+                        using (var contactCmd = new NpgsqlCommand("CALL GetContacts(@p_ContactId, @p_ContactName)", npgsqlConnection, transaction))
                         {
                             contactCmd.CommandType = CommandType.Text;
                             contactCmd.Parameters.Add("p_ContactId", NpgsqlDbType.Integer).Value = id;
@@ -214,11 +302,34 @@ namespace WebApplication2.Controllers
             using (NpgsqlConnection npgsqlConnection = new NpgsqlConnection(pgsqlConnection))
             {
                 npgsqlConnection.Open();
+
+                IActionResult result = GetDetails(Contacts.ContactId);
+                string json = "";
+
+                if (result is OkObjectResult okResult)
+                {
+                    json = JsonSerializer.Serialize(okResult.Value);
+                }
+
+                List<int> dBContactId = new List<int>();
+
+                if (!string.IsNullOrEmpty(json))
+                {
+                    var contactDetails = JsonSerializer.Deserialize<Newcontactresponse>(json);
+
+                    if (contactDetails != null && contactDetails.ListOfNumbers != null)
+                    {
+                        dBContactId = contactDetails.ListOfNumbers.Select(n => n.NumberId).ToList();
+                    }
+                }
+                List<int> swagNumberId = Contacts.Alternewcontact.Select(n => n.NumberId).ToList();
                 DateTime createdOn = Contacts.CreatedOn == default ? DateTime.UtcNow : Contacts.CreatedOn;
 
-                using (NpgsqlCommand cmd = new NpgsqlCommand(@"call UpdateContact(@p_ContactId,@p_ContactName, @p_Relation, @p_Gender, @p_IsDeleted, @p_CreatedOn)", npgsqlConnection))
+                using (NpgsqlCommand cmd = new NpgsqlCommand(@"call UpdateContact(@p_ContactId,@p_ContactName,@p_Relation,@p_Gender,@p_IsDeleted,@p_CreatedOn)", npgsqlConnection))
                 {
                     cmd.CommandType = CommandType.Text;
+                    cmd.Parameters.Add("@p_ContactId", NpgsqlDbType.Integer).Value = Contacts.ContactId;
+
                     cmd.Parameters.Add("@p_ContactName", NpgsqlDbType.Varchar).Value = Contacts.ContactName;
                     cmd.Parameters.Add("@p_Gender", NpgsqlDbType.Varchar).Value = Contacts.Gender;
                     cmd.Parameters.Add("@p_Relation", NpgsqlDbType.Varchar).Value = Contacts.Relation;
@@ -226,26 +337,22 @@ namespace WebApplication2.Controllers
                     cmd.Parameters.Add("@p_CreatedOn", NpgsqlDbType.TimestampTz).Value = createdOn;
 
 
-                    var id = new NpgsqlParameter("@p_ContactId", NpgsqlDbType.Integer)
-                    {
-                        Direction = ParameterDirection.InputOutput,
-                        Value = Contacts.ContactId
 
-                    };
-                    cmd.Parameters.Add(id);
                     cmd.ExecuteNonQuery();
-                    ContactId = id.Value != DBNull.Value ? Convert.ToInt32(id.Value) : 0;
 
                 }
+
+
+
                 foreach (var n in Contacts.Alternewcontact)
                 {
-                    if (.Contains(n.NumberId))
+                    if (dBContactId.Contains(n.NumberId))
                     {
 
-                        using (NpgsqlCommand cmd = new NpgsqlCommand(@"call CreateNumber(@p_ContactId,@p_PhoneNumber)", npgsqlConnection))
+                        using (NpgsqlCommand cmd = new NpgsqlCommand(@"call UpdateNumber(@p_ContactId,@p_PhoneNumber)", npgsqlConnection))
                         {
                             cmd.CommandType = CommandType.Text;
-                            cmd.Parameters.Add("@p_ContactId", NpgsqlDbType.Integer).Value = ContactId;
+                            cmd.Parameters.Add("@p_ContactId", NpgsqlDbType.Integer).Value = Contacts.ContactId;
                             cmd.Parameters.Add("@p_PhoneNumber", NpgsqlDbType.Varchar).Value = n.PhoneNumber;
 
                             cmd.ExecuteNonQuery();
@@ -256,7 +363,7 @@ namespace WebApplication2.Controllers
                         using (NpgsqlCommand cmd = new NpgsqlCommand(@"call CreateNumber(@p_ContactId,@p_PhoneNumber)", npgsqlConnection))
                         {
                             cmd.CommandType = CommandType.Text;
-                            cmd.Parameters.Add("@p_ContactId", NpgsqlDbType.Integer).Value = ContactId;
+                            cmd.Parameters.Add("@p_ContactId", NpgsqlDbType.Integer).Value = Contacts.ContactId;
                             cmd.Parameters.Add("@p_PhoneNumber", NpgsqlDbType.Varchar).Value = n.PhoneNumber;
                             cmd.ExecuteNonQuery();
                         }
@@ -264,8 +371,99 @@ namespace WebApplication2.Controllers
                     }
 
                 }
+
+
+                List<int> DeleteId = dBContactId.Except(swagNumberId).ToList();
+                foreach (var id in DeleteId)
+                {
+                    using (NpgsqlCommand cmd = new NpgsqlCommand(@"call DeleteUpdateNumber(@p_NumberId)", npgsqlConnection))
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Parameters.Add("p_NumberId", NpgsqlDbType.Integer).Value = id;
+                        cmd.ExecuteNonQuery();
+                    }
+
+                }
+                npgsqlConnection.Close();
+
             }
+
+
+
             return Ok("Updated sucessfully");
         }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAllDetails(int id)
+        {
+            string? pgsqlConnection = _configuration.GetConnectionString("DefaultConnection");
+
+            using (var npgsqlConnection = new NpgsqlConnection(pgsqlConnection))
+            {
+                npgsqlConnection.Open();
+
+                string contactName;
+                using (var contactCmd = new NpgsqlCommand("CALL GetContacts(@p_ContactId, @p_ContactName)", npgsqlConnection))
+                {
+                    contactCmd.Parameters.AddWithValue("p_ContactId", id);
+                    var contactNameParam = new NpgsqlParameter("p_ContactName", NpgsqlDbType.Varchar)
+                    {
+                        Direction = ParameterDirection.InputOutput,
+                        Value = DBNull.Value
+                    };
+                    contactCmd.Parameters.Add(contactNameParam);
+
+                    contactCmd.ExecuteNonQuery();
+                    contactName = contactNameParam.Value?.ToString() ?? "Not Defined";
+                }
+
+                // Get all phone numbers
+                var numberDetails = new List<Alternewcontactresponse>();
+                using (var Cmd = new NpgsqlCommand("CALL GetAllNumbers(@p_ContactId, @getCursor)", npgsqlConnection))
+                {
+                    Cmd.Parameters.AddWithValue("p_ContactId", id);
+                    var cursorParam = new NpgsqlParameter("getCursor", NpgsqlDbType.Refcursor)
+                    {
+                        Direction = ParameterDirection.InputOutput,
+                        Value = "getCursor"
+                    };
+                    Cmd.Parameters.Add(cursorParam);
+                    Cmd.ExecuteNonQuery();
+
+
+                    using (var fetchCmd = new NpgsqlCommand("FETCH ALL FROM getCursor;", npgsqlConnection))
+                    using (var reader = fetchCmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            numberDetails.Add(new Alternewcontactresponse
+                            {
+                                NumberId = reader.GetInt32(1),
+                                PhoneNumber = reader.GetString(0)
+                            });
+                        }
+                    }
+
+                    // Close the cursor
+                    using (var closeCmd = new NpgsqlCommand("CLOSE getCursor;", npgsqlConnection))
+                    {
+                        closeCmd.ExecuteNonQuery();
+                    }
+                }
+
+                var contactDetails = new Newcontactresponse
+                {
+                    ContactName = contactName,
+                    ListOfNumbers = numberDetails
+                };
+
+                return Ok(contactDetails);
+            }
+        }
+
+
+
+
+
     }
 }
